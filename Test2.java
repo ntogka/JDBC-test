a25e674bf5be06be95787f0bd9295a9ab3f5e937
Marina Ntogka <marinadoga98@gmail.com>
2021-12-09 15:29:06 +0200

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -1,6 +1,8 @@
 import oracle.jdbc.pool.OracleDataSource;
 
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Test2 {
@@ -10,6 +12,7 @@ public class Test2 {
         PreparedStatement pst = null;
         Scanner scanner = null;
         ResultSet rs = null;
+        Statement st = null;
 
         try {
             OracleDataSource ds = new OracleDataSource();
@@ -17,13 +20,32 @@ public class Test2 {
             ds.setUser("modesto");
             ds.setPassword("modesto");
             con = ds.getConnection();
+            int parentId = 0;
+            int detailId = 0;
 
-            pst = con.prepareStatement("insert into party (PR_ID)" +
-                    "select party_seq.nextval from dual;");
-            pst = con.prepareStatement("insert into PARTY_DETAILS (PRD_ID)" +
-                    "select party_details_seq.nextval from dual;");
-            pst = con.prepareStatement("insert into PARTY_DETAILS (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            // create a sql date object
+            Calendar calendar = Calendar.getInstance();
+            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
 
+            st = con.createStatement();
+
+
+            rs = st.executeQuery("select party_seq.nextval as newVal1 from dual");
+            while(rs.next()) {
+                parentId = rs.getInt("newVal1");
+                System.out.println(parentId);
+            }
+
+
+            rs = st.executeQuery("select party_details_seq.nextval as newVal2 from dual");
+            while(rs.next()) {
+                detailId = rs.getInt("newVal2");
+                System.out.println(detailId);
+            }
+            String masterQuery = "insert into PARTY (pr_id) values (" + parentId + ")";
+
+            String query = "insert into PARTY_DETAILS (prd_id,prd_pr_id,prd_name_en, prd_eic, prd_effective_date) values (" + detailId + "," + parentId + ",?,?,?)";
+            //System.out.println(query);
             scanner = new Scanner(System.in);
 
             while (true) {
@@ -35,15 +57,21 @@ public class Test2 {
 
                 System.out.print("Give me the Effective Date ");
                 String prd_effective_date  = scanner.next();
+                java.sql.Date sqlDate = parseUserDate(prd_effective_date);
+                if(sqlDate==null)
+                    sqlDate = startDate;
 
-                //pst.setInt(1, Integer.parseInt("newValue"));
-                pst.setString(4, prd_name_en);
+                pst = con.prepareStatement(masterQuery);
+                int rowCountMaster = pst.executeUpdate();
+                System.out.println("rowCountMaster = "+rowCountMaster);
+                pst = con.prepareStatement(query);
+                pst.setString(1, prd_name_en);
                 pst.setString(2, prd_eic);
-                pst.setString(13, prd_effective_date); //error an einai Date
-                //pst.setInt(5, Integer.parseInt("new"));
+                pst.setDate(3, sqlDate);
 
-                int rowCount = pst.executeUpdate(); //error
-                if( rowCount == 1) {
+
+                int rowCount = pst.executeUpdate();
+                if(rowCount == 1) {
                     System.out.println(prd_name_en + "Inserted Successfully");
                     System.out.println("One more Insertion? [Yes/No]    : ");
                     String option = scanner.next();
@@ -65,12 +93,25 @@ public class Test2 {
         }
         finally {
             try {
-                //scanner.close();
+                scanner.close();
                 pst.close();
                 con.close();
             } catch (Exception e){
                 e.printStackTrace();
             }
+        }
+
+    }
+
+    public static java.sql.Date parseUserDate(String dt){
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); //09/12/2021
+            java.util.Date myDate = sdf.parse(dt);
+            java.sql.Date sqlDate = new java.sql.Date(myDate.getTime());
+            return sqlDate;
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
         }
 
     }
