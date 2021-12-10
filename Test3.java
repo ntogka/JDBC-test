e9cad6c3b5f670b82f8971b8b000cc65f2bbb870
Marina Ntogka <marinadoga98@gmail.com>
2021-12-10 14:28:12 +0200

    Update Test3.java

--- Test3.java
+++ Test3.java
@@ -3,7 +3,6 @@ import oracle.jdbc.pool.OracleDataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Scanner;
@@ -16,7 +15,6 @@ public class Test3 {
         Scanner scanner = null;
         ResultSet rs = null;
 
-
         try {
             OracleDataSource ds = new OracleDataSource();
             ds.setURL("jdbc:oracle:thin:@db12c.qnr.com.gr:1521/p12c.qnr.com.gr");
@@ -25,19 +23,19 @@ public class Test3 {
             con = ds.getConnection();
 
             int id = 0;
+            int mtrId = 0;
 
             Calendar calendar = Calendar.getInstance();
             java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
 
             String q1 = "select wec_id from work_energy_control where wec_trade_date = ? and wec_version = ?";
-
-            String query1 = "delete from work_energy where we_wec_id = ? ";
-            String query2 = "update work_energy set we_pa_energy = we_pa_energy + ? and we_ma_energy = we_ma_energy + ? where we_wec_id = ?";
+            String q2 = "select mtrd_mtr_id from meter_details where mtrd_rtu = ? and mtrd_main_check = 'M'";
+            String query1 = "delete from work_energy where we_wec_id = ? and we_hour = ? and we_mtr_id = ?";
+            String query2 = "update work_energy set we_pa_energy = we_pa_energy + ?, we_ma_energy = we_ma_energy + ? where we_wec_id = ? and we_hour = ? and we_mtr_id = ?";
 
             scanner = new Scanner(System.in);
 
-            while (true) {
-                System.out.print("Date : ");
+            System.out.print("Date : ");
                 String date = scanner.next();
                 java.sql.Date sqlDate = parseUserDate(date);
                 if (sqlDate == null)
@@ -45,85 +43,120 @@ public class Test3 {
 
                 System.out.print("Object Version [1/2/3]: ");
                 int version = scanner.nextInt();
-                if (version == 1 || version == 2 || version == 3) {
-                    System.out.println("Good");
-                }else {
-                    System.out.println("Invalid version. Try again.");
-                    version = scanner.nextInt();
+                int i = 0;
+                while (i == 0) {
+                    if (version == 1 || version == 2 || version == 3){
+                        System.out.println("Good");
+                        i ++;
+                    }else {
+                        System.out.print("Invalid version. Try again : ");
+                        version = scanner.nextInt();
+                    }
                 }
 
-                System.out.print("RTU time : ");
+                System.out.print("RTU : ");
+                String rtu = scanner.next();
+                if (rtu.equals("0")) {
+                    System.exit(0);
+                }
+
+                System.out.print("Time : ");
                 String hour = scanner.next();
-                /*if (hour.matches("([01]?[0-9]|2[0-3]):[00]|[15]|[30]|[45]")) {
-                    System.out.println("Good");
-                } else {
-                    System.out.println("Invalid RTU time. Try again.");
-                    hour = scanner.next();
+
+                /*int m = 0;
+                while (m == 0){
+                    if (hour.matches("([01]?[0-9]|2[0-3]):[0]|[15]|[30]|[45]")) { //error
+                        //if (hour.matches("^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0]|[15]|[30]|[45]$")) { //HH:MM 24-hour format, optional leading 0
+                        System.out.println("Good");
+                        m++;
+                    } else {
+                        System.out.println("Invalid time. Try again.");
+                        hour = scanner.next();
+                    }
                 }*/
 
+
                 pst = con.prepareStatement(q1);
-                pst.setString(1, date);
+                pst.setDate(1, sqlDate);
                 pst.setInt(2, version);
                 rs = pst.executeQuery();
-                System.out.println(q1);
                 while (rs.next()) {
                     id = rs.getInt("wec_id");
                     System.out.println(id);
                 }
 
+                pst = con.prepareStatement(q2);
+                pst.setString(1, rtu);
+                rs = pst.executeQuery();
+                while (rs.next()) {
+                    mtrId = rs.getInt("mtrd_mtr_id");
+                    System.out.println(mtrId);
+                }
+
                 System.out.print("Action [U/D]: ");
                 String action = scanner.next();
+                int y = 0;
+                while (y == 0) {
+                    if (action.equalsIgnoreCase("U")) {
 
-                if (action.equalsIgnoreCase("U")) {
+                        System.out.print("+A or -A: ");
+                        String A = scanner.next();
+                        System.out.print("Price: ");
+                        int price = scanner.nextInt();
+                        int zero = 0;
+                        int z = 0;
+                        while (z == 0) {
+                            if (A.equalsIgnoreCase("+A")) {
+                                pst = con.prepareStatement(query2);
+                                pst.setInt(1, price);
+                                pst.setInt(2, zero);
+                                pst.setInt(3, id);
+                                pst.setString(4, hour);
+                                pst.setInt(5, mtrId);
 
-                    System.out.print("+A or -A: ");
-                    String A = scanner.next();
-                    System.out.print("Price: ");
-                    int price = scanner.nextInt();
-                    int zero = 0;
 
-                    if (A.equalsIgnoreCase("+A")) {
-                        pst = con.prepareStatement(query2);
-                        pst.setInt(1, price);
-                        pst.setInt(2, zero);
-                        pst.setInt(3, id);
+                                int rowCountpA = pst.executeUpdate();
+                                System.out.println("rowCountpAUpdate = " + rowCountpA);
+                                System.out.println("Updated");
+                                z++;
+                            } else if (A.equalsIgnoreCase("-A")) {
+                                pst = con.prepareStatement(query2);
+                                pst.setInt(1, zero);
+                                pst.setInt(2, price);
+                                pst.setInt(3, id);
+                                pst.setString(4, hour);
+                                pst.setInt(5, mtrId);
 
-                        System.out.println(query2);
-                        int rowCount2 = pst.executeUpdate();
-                        System.out.println("rowCount2 = " + rowCount2);
-                        System.out.println("Updated");
-                        break;
-                    } else if (A.equalsIgnoreCase("-A")) {
-                        pst = con.prepareStatement(query2);
-                        pst.setInt(1, zero);
-                        pst.setInt(2, price);
-                        pst.setInt(3, id);
+                                //System.out.println(query2);
+                                int rowCountmA = pst.executeUpdate();
+                                System.out.println("rowCountmAUpdate = " + rowCountmA);
+                                System.out.println("Updated");
+                                z++;
+                            } else {
+                                System.out.print("Invalid. Try again [-A or +A] : ");
+                                A = scanner.next();
+                            }
+                        }
+                        y++;
 
-                        System.out.println(query2);
-                        int rowCount3 = pst.executeUpdate();
-                        System.out.println("rowCount2 = " + rowCount3);
-                        System.out.println("Updated");
-                        break;
+                    } else if (action.equalsIgnoreCase("D")) {
+
+                        pst = con.prepareStatement(query1);
+                        pst.setInt(1, id);
+                        pst.setString(2, hour);
+                        pst.setInt(3, mtrId);
+
+                        if (pst.execute()) {
+                            System.out.println("Deleted");
+                        }
+
+                        y++;
                     } else {
-                        System.out.println("Invalid. Try again.");
-                        A = scanner.next();
+                        System.out.print("Invalid. Try again: ");
+                        action = scanner.next();
                     }
-
-                } else if (action.equalsIgnoreCase("D")) {
-
-                    pst = con.prepareStatement(query1);
-                    pst.setInt(1, id);
-                    System.out.println(query1);
-                    int rowCount1 = pst.executeUpdate();
-                    System.out.println("rowCount1 = " + rowCount1);
-
-                    System.out.println("Deleted");
-                    break;
-                } else {
-                    System.out.println("Invalid. Try again.");
-                    action = scanner.next();
                 }
-            }
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -154,6 +187,3 @@ public class Test3 {
 
 
 }
-
-
-
